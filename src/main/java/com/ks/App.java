8703d9eda285b0252d6d8283baca3729e6fe6f2b
Joel <jmedina@kssoluciones.com.mx>
2018-05-21 17:36:16 -0500

    Trabajando

--- src/main/java/com/ks/App.java
+++ src/main/java/com/ks/App.java
@@ -1,15 +1,12 @@
 package com.ks;
 
 import com.ks.Threads.TestThreads;
-
 public class App {
+
     public static void main(String[] args) {
-<<<<<<< HEAD
-=======
-        //new Thread(new TestThreads()).run();
->>>>>>> b2e15004b5a25e44c59b73f2462150c337780e92
+
         TestThreads testThreads = new TestThreads();
-        testThreads.AgregarCola();
-        testThreads.InsertarBd();
+        testThreads.start();
+
     }
 }
--- src/main/java/com/ks/Threads/TestThreads.java
+++ src/main/java/com/ks/Threads/TestThreads.java
@@ -1,28 +1,19 @@
 package com.ks.Threads;
 
+import com.ks.lib.Configuracion;
+import org.apache.logging.log4j.*;
+
+import java.io.*;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.LinkedList;
+import java.util.Properties;
 import java.util.Queue;
 
+
 public class TestThreads {
-    /*un hilo tiene cuatro estados:
-        +recien creada -> se le asignan datos
-        +corrible -> (Runnable) ocupa la cpu en cualquier momento
-            -si en este paso se ocupa yield pasa a ultimo lugar pero aun en corriendo
-        +bloqueada -> se invoca el metodo suspend() y por herencia se corre el metodo
-                      sleep el hilo invoca join() al hilo corriendo y espera a que
-                      termine el evento para inmediatamente ejecutarse al termino de este
-         +muerta-> acaba la ejecucion del hilo u otro hilo ejecuta el metodo stop del otro hilo
-                    es recomendable usar safeStop() para evitar inconcistencias
-
-          Cuando un hilo llama a wait(), la llave que éste tiene es liberada, así otro proceso
-          que esperaba por ingresar al monitor puede hacerlo. notify() sólo despierta o desbloquea
-          un hilo, si lo hay esperando. notifyAll() despierta a todos los que estén esperando
-
-    */
     private Thread hiloLlenar;
     private Thread HiloInsertarD;
     private Connection connection;
@@ -30,18 +21,24 @@ public class TestThreads {
     public int id;
     public dato Datos;
     public Statement statement;
+    public Logger Depuracion = LogManager.getLogger(TestThreads.class.getName());
 
-    public TestThreads() {
+    public void start() {
+        Depuracion.info("Instanciando los hilos");
         hiloLlenar = new Thread();
         HiloInsertarD = new Thread();
         cola = new LinkedList();
         id = 1;
         try {
             connection = conectarBD();
-            statement=connection.createStatement();
+            statement = connection.createStatement();
         } catch (Exception e) {
             e.printStackTrace();
         }
+        Depuracion.info("Iniciando los hilos");
+        AgregarCola();
+        InsertarBd();
+        Depuracion.error("Error de prueba");
     }
 
 
@@ -49,7 +46,7 @@ public class TestThreads {
         if (!hiloLlenar.isAlive()) {
             hiloLlenar = new Thread(() -> {
                 try {
-                    long tolerancia = System.currentTimeMillis() + (10000);
+                    long tolerancia = System.currentTimeMillis() + (8000);
                     do {
                         dato Dato = new dato(id, String.valueOf((int) (Math.random() * 100) + 1));
                         synchronized (cola) {
@@ -75,13 +72,13 @@ public class TestThreads {
                     if (!cola.isEmpty()) {
                         synchronized (cola) {
                             try {
-                                Datos= (dato) cola.poll();
-                                int id=Datos.getId();
-                                String valor=Datos.getDato();
-                                statement.execute("INSERT INTO datos values ("+id+",'"+valor+"')");
-                                System.out.println("se ha guardado id: "+id+" con valor: "+valor);
+                                Datos = (dato) cola.poll();
+                                int id = Datos.getId();
+                                String valor = Datos.getDato();
+                                statement.execute("INSERT INTO datos values (" + id + ",'" + valor + "')");
+                                System.out.println("se ha guardado id: " + id + " con valor: " + valor);
                             } catch (Exception e) {
-                                e.printStackTrace();
+                                Depuracion.error("Error al insertar datos: " + e.getMessage());
                             }
                         }
                     }
@@ -94,9 +91,19 @@ public class TestThreads {
 
     public Connection conectarBD() {
         try {
-            return connection = DriverManager.getConnection("jdbc:sqlite:C:\\Users\\joela\\IdeaProjects\\javp\\MisPruebas\\sqlite\\base.db", "", "");
+
+            Properties properties = new Properties();
+            String pathconfiguracion = Configuracion.getRutaConfiguracion() + System.getProperty("propertiesName") + ".properties";
+            Depuracion.info("Se esta conectando a la base de datos");
+            File fileproperties = new File(pathconfiguracion);
+            InputStream propiedades = new FileInputStream(fileproperties);
+            properties.load(propiedades);
+            String ruta = properties.getProperty("path");
+            return connection = DriverManager.getConnection("jdbc:sqlite:" + ruta, "", "");
         } catch (SQLException e) {
-            e.printStackTrace();
+            Depuracion.error("Error en la base de datos: " + e.getMessage());
+        } catch (Exception e) {
+            Depuracion.error("Error en la conexion a base de datos: " + e.getMessage());
         }
         return null;
     }
